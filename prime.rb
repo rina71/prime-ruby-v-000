47add1d35b3bf6234e4354e292a3830363edd3e4
Yurina Shin <yurinas@gmail.com>
2017-12-10 20:03:54 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,9 @@
 def prime?(num)
-  if num > 1 && (num % 2 == 0 && num % 3 == 0 && num % 5 == 0)
+  if num > 1 && 
+  elsif (2..num/2).none?{|i| i == 0 } 
     true
   else
     false
   end
+end
 end
