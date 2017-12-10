014848988d5288caf224b2e0d827303573da81de
Yurina Shin <yurinas@gmail.com>
2017-12-10 20:06:54 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 def prime?(num)
-  if num > 1
+  if num > 1 && (2..num/2).none?{|i| num % i == 0 }
     true
-  elsif (2..num/2).none?{|i| num % i == 0 }
-    true
+  # elsif (2..num/2).none?{|i| num % i == 0 }
+  #   true
   else
     false
   end
